1d70dc979be5c879452329f923928937e07ea3a6
AugustoDeve <augsuto.aas@gmail.com>
2020-02-13 11:06:50 -0300

    Projeto desenvolvido em aula - COTI Informatica

---
+++ Projeto.Data/Contracts/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Data.Contracts
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        void Inserir(T obj);
+        void Alterar(T obj);
+        void Excluir(T obj);
+
+        List<T> ConsultarTodos();
+        T ConsultarPorId(int id);
+    }
+}
---
+++ Projeto.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Projeto.Data.Contracts;
+using Projeto.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Data.Contexts;
+using System.Linq;
+
+namespace Projeto.Data.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+        private DataContext context;
+
+        public ProdutoRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public void Inserir(Produto obj)
+        {
+            context.Entry(obj).State = EntityState.Added;//grava
+            context.SaveChanges();
+        }
+
+        public void Alterar(Produto obj)
+        {
+            context.Entry(obj).State = EntityState.Modified;//att
+            context.SaveChanges();
+        }
+
+        public void Excluir(Produto obj)
+        {
+            context.Entry(obj).State = EntityState.Deleted;//excluir
+            context.SaveChanges();
+        }
+
+        public List<Produto> ConsultarTodos()
+        {
+            return context.Set<Produto>().ToList();
+        }
+
+        public Produto ConsultarPorId(int id)
+        {
+            return context.Set<Produto>().Find(id);
+        }
+    }
+}
